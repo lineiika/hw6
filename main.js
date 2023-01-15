503ce178857abe28b7c405082d1a319d37470d8a
lineiika <avramenko.liana@gmail.com>
2023-01-15 17:38:50 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,117 @@
+
+// 1
+
+// let str = '10';
+// for(let i = 11; i <= 20; i++) {
+// str = str + ','+ i ;
+// }
+
+// document.write(str);
+
+
+// 2
+
+// for(let i = 10; i <= 20; i = i +1){
+//     document.write(`${i} = ${i * i} <br>`);
+// }
+
+
+// 3
+
+// for (let i=7; i<=7; i++) {
+//      let j = 1;
+  
+// while (j<=10) {
+//      document.write(i + " * " + j + " = " + i*j + "<br>");
+//       j++;
+//     }
+// }
+
+
+// 4
+
+// let sum = 0;
+// for(let i = 1; i <= 15; i = i +1) {
+//     sum = sum + i;
+// }
+
+// document.write(sum);
+
+
+// 5
+
+// let sum = 1;
+// for(let i = 15; i <= 35; i = i + 1) {
+
+//     sum = sum * i;
+// }
+
+// console.log(sum);
+
+
+// 6
+
+// let sum = 0;
+// for(let i = 1; i <= 500; i = i +1) {
+//     sum = sum + i;
+// }
+
+// document.write(sum / 500);
+
+
+// 7
+
+// let sum = 0;
+// for (let i = 30; i <= 80; i = i + 1) {
+//     if (i % 2 === 0) {
+//         document.write(i +'<br>');  
+//         sum = sum + i;
+//     }
+// }
+// document.write(sum);
+
+
+// 8
+
+// let sum = 0;
+// for (let i = 100; i <= 200; i = i + 1) {
+//     if (i % 3 === 0) {
+//         document.write(i +'<br>');  
+//     }  
+// }
+
+
+// 9,10,11
+
+// let userAnswer = Number(prompt('Enter your number'));
+// let numOfDev = 0;
+// let sumOfDev = 0;
+
+// for (let i = 1; i <= userAnswer; i ++){
+//         if (userAnswer % i === 0) {
+//             document.write(i +'<br>');
+
+//             if(i % 2 === 0) {
+//                 numOfDev = numOfDev + 1;
+//                 sumOfDev = sumOfDev + i;
+//             }
+//         }
+//        } 
+//        document.write (`Your number : ${userAnswer} <br> 
+//        Even Devisors : ${numOfDev} <br> 
+//        Amount of Devisors : ${sumOfDev}`)
+
+       
+
+
+// 12
+// for (let i=1; i<=10; i++) {
+//      let j = 1;
+  
+//     while (j<=10) {
+//       document.write(i + " * " + j + " = " + i*j + "<br>");
+//       j++;
+//     }
+//   }
+
+
